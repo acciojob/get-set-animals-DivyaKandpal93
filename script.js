ff2e1329710b7b2af6ba79bba9ae13c331874988
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-02-18 20:26:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-//complete this code
-class Animal {}
+class Animal{
+    constructor(species){
+this.species = species;
+    }
+    
+    makeSound(){
+        return console.log(`The ${this.species} makes a sound`);
+    }
+}
+class Cat extends Animal{
+constructor(species){
+    super(species);
+}
+purr(){
+return console.log("purr");
+}
+}
+class Dog extends Animal{
+constructor(species){
+    super(species);
+}
+bark(){
+    return console.log("woof");
+}
+}
 
-class Dog extends Animal {}
-
-class Cat extends Animal {}
+const anm = new Dog("Dog");
+anm.makeSound();
+anm.bark();
+const anm2 = new Cat("Cat");
+anm2.makeSound();
+anm2.purr();
 
 // Do not change the code below this line
 window.Animal = Animal;
